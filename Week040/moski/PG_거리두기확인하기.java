3d92fa54ed4129041b9e22e5a31c1c93af37d5a6
rudgns9334 <rudgns9334@gmail.com>
2023-09-20 10:18:08 +0900

    정경훈 : 40주차

--- Week040/moski/PG_거리두기확인하기.java
+++ Week040/moski/PG_거리두기확인하기.java
@@ -5,6 +5,7 @@ public class PG_거리두기확인하기 {
     static int[] dy = {-1, 1, 0, 0};
     static int[] dx = {0, 0, -1, 1};
 
+
     public int[] solution(String[][] places) {
         int[] answer = new int[places.length];
 
@@ -24,9 +25,9 @@ public class PG_거리두기확인하기 {
             while(!q.isEmpty()){
                 int[] node = q.poll();
 
-                for(int d1=0;d1<4;d1++){
-                    int ny = node[0] + dy[d1];
-                    int nx = node[1] + dx[d1];
+                for(int d=0;d<4;d++){
+                    int ny = node[0] + dy[d];
+                    int nx = node[1] + dx[d];
 
                     if(ny<0 || nx<0 || ny>=5 || nx>=5) continue;
                     if(map[ny][nx] == 'P'){
@@ -34,22 +35,26 @@ public class PG_거리두기확인하기 {
                         break;
                     }
                     if(map[ny][nx] == 'O'){
-                        for(int d2=0;d2<4;d2++){
-                            int nny = ny + dy[d2];
-                            int nnx = nx + dx[d2];
+                        for(int d1=0;d1<4;d1++){
+                            int nny = ny + dy[d1];
+                            int nnx = nx + dx[d1];
                             if(nny == node[0] && nnx == node[1]) continue;
                             if(nny<0 || nnx<0 || nny>=5 || nnx>=5) continue;
-                            if(map[ny][nx] == 'P'){
+                            if(map[nny][nnx] == 'P'){
                                 ok = false;
                                 break;
                             }
                         }
+
                     }
                 }
                 if(!ok) break;
             }
 
+            if(ok) answer[i] = 1;
+
         }
 
         return answer;
+    }
 }
