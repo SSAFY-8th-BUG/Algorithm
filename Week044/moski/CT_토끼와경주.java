f527c50887cea3873247246a430fed019e02afb3
rudgns9334 <rudgns9334@gmail.com>
2023-10-14 03:31:39 +0900

    정경훈 : 44주차

--- Week044/moski/CT_토끼와경주.java
+++ Week044/moski/CT_토끼와경주.java
@@ -3,50 +3,45 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class CT_토끼와경주 {
-    static final int MAX_P = 10_000_000;
-    static int Q, P, N, M;
-    static int maxScore;
+    static final int MAX_N = 2000;
 
-    // 맵 배열
-    static int[][] map;
+    static int Q, P, N, M;
+    static long totalSum;
+
+    static int[] p = new int[MAX_N+1];
+
+    static int[] d = new int[MAX_N+1];
+
+    static int[] jump = new int[MAX_N+1];
+
+    static long[] result = new long[MAX_N+1];
+
+    static Node[] point = new Node[MAX_N+1];
+
+    static HashMap<Integer, Integer> pToIdx = new HashMap<>();
+
+    static boolean[] isMove = new boolean[MAX_N+1];
+
 
     // 토끼 정보를 저장할 배열
     static Rabbit[] rabbits;
 
     static PriorityQueue<Rabbit> rabbitPq = new PriorityQueue<>((o1, o2)->{
-        if(o1.cnt == o2.cnt){
-            if(o1.y + o1.x == o2.y + o2.x){
-                if(o1.y == o2.y){
-                    if(o1.x == o2.x){
-                        return o1.p - o2.p;
-                    }else{
-                        return o1.x - o2.x;
-                    }
-                }else{
-                    return o1.y - o2.y;
-                }
-            }else{
-                return (o1.y + o1.x) - (o2.y + o2.x);
-            }
+        if(o1.cnt != o2.cnt){
+            return o1.cnt - o2.cnt;
         }
-        return o1.cnt - o2.cnt;
+        if(o1.y + o1.x != o2.y + o2.x){
+            return (o1.y + o1.x) - (o2.y + o2.x);
+        }
+        if(o1.y != o2.y){
+            return o1.y - o2.y;
+        }
+        if(o1.x != o2.x){
+            return o1.x - o2.x;
+        }
+        return o1.p - o2.p;
     });
 
-    static PriorityQueue<Rabbit> bonusRabbit = new PriorityQueue<>((o1, o2) -> {
-        if((o1.y+o1.x) != (o2.y+o2.x)) {
-            return (o2.y + o2.x) > (o1.y + o1.x) ? 1 : -1;
-        }
-        if(o2.y != o1.y) {
-            return o2.y > o1.y ? 1 : -1;
-        }
-
-        if(o2.x != o1.x) {
-            return o2.x > o1.x ? 1 : -1;
-        }
-
-        return o2.p > o1.p ? 1 : -1 ;
-
-    });
 
     static PriorityQueue<Node> nodePq = new PriorityQueue<>();
     static int[] dy = {-1, 1, 0, 0};
@@ -66,17 +61,18 @@ public class CT_토끼와경주 {
                     M = Integer.parseInt(st.nextToken());
                     P = Integer.parseInt(st.nextToken());
 
-                    maxScore = 0;
-                    map = new int[N+1][M+1];
+                    totalSum = 0;
 
                     rabbits = new Rabbit[P+1];
 
                     for(int j=1;j<=P;j++){
-                        int p = Integer.parseInt(st.nextToken());
-                        int d = Integer.parseInt(st.nextToken());
-                        rabbits[j] = new Rabbit(p, d, 0, 0, 1, 1);
-                        rabbitPq.add(rabbits[j]);
-                        bonusRabbit.add(rabbits[j]);
+                        int pp = Integer.parseInt(st.nextToken());
+                        int dd = Integer.parseInt(st.nextToken());
+//                        rabbits[j] = new Rabbit(pp, dd, 0, 0, 1, 1);
+                        p[j] = pp;
+                        d[j] = dd;
+                        pToIdx.put(p[j], j);
+                        point[j] = new Node(1,1);
                     }
                     break;
                 case 200:
@@ -97,134 +93,252 @@ public class CT_토끼와경주 {
     }
 
     static void start(int k, int s){
+
+        rabbitPq.clear();
+
+//        for(int i=1;i<=P;i++){
+//            rabbits[i].isMove = false;
+//            rabbitPq.add(rabbits[i]);
+//        }
+
+        for(int i=1;i<=P;i++){
+            isMove[i] = false;
+            rabbitPq.add(new Rabbit(p[i], jump[i], point[i].y, point[i].x));
+        }
+
         // K번 반복
         for(int i=0;i<k;i++){
             // 우선순위가 높은 토끼 차출
             Rabbit rabbit = rabbitPq.poll();
 
-            int d = rabbit.d;
-
             // 우선 순위를 뽑아줄 nodePq 초기화
             nodePq.clear();
+             nodePq.add(new Node(0, 0));
+//            Node node = new Node(0, 0);
             // 상하좌우 순
             for(int j=0;j<4;j++){
                 int ny = rabbit.y;
                 int nx = rabbit.x;
-                int nd = d;
+//                int nd = rabbit.d;
+                int nd = d[pToIdx.get(rabbit.p)];
                 switch (j){
                     case 0: // 상
-                        nd = d % (N * 2 - 2);
+                        nd %= (N * 2 - 2);
                         // 이동 거리가 벽에 닿이지 않는 경우
-                        if(nd <= ny - 1){
-                            ny = ny + nd * dy[j];
-                        }else if(nd <= ny + N - 2){ // 한번 방향 튼 경우
-                            ny = 1 + (nd - (ny - 1)) * dy[j+1];
-                        }else{ // 두번 방향 튼 경우
-                            ny = N + (nd -(ny + N - 2)) * dy[j];
+                        if(nd >= ny - 1) {
+                            nd -= ny - 1;
+                            ny = 1;
+                        }else{
+                            ny += nd * dy[j];
+                            nd = 0;
                         }
+                        if(nd >= N - ny){ // 한번 방향 튼 경우
+                            nd -= N - ny;
+                            ny = N;
+                        }else{
+                            ny += nd * dy[j+1];
+                            nd = 0;
+                        }
+                        ny += nd * dy[j];
                         break;
                     case 1: // 하
-                        nd = d % (N * 2 - 2);
+                        nd %= (N * 2 - 2);
                         // 이동 거리가 벽에 닿이지 않는 경우
-                        if(nd <= N - ny){
-                            ny = ny + nd * dy[j];
-                        }else if(nd <= N * 2 - ny - 1){ // 한번 방향 튼 경우
-                            ny = N + (nd - (N - ny)) * dy[j-1];
-                        }else{ // 두번 방향 튼 경우
-                            ny = 1 + (nd -(N * 2 - ny - 1)) * dy[j];
+                        if(nd >= N - ny){
+                            nd -= N - ny;
+                            ny = N;
+                        }else{
+                            ny += nd * dy[j];
+                            nd = 0;
                         }
+                        if(nd >= ny - 1){ // 한번 방향 튼 경우
+                            nd -= ny - 1;
+                            ny = 1;
+                        }else{
+                            ny += nd * dy[j-1];
+                            nd = 0;
+                        }
+                        ny += nd * dy[j];
                         break;
                     case 2:
-                        nd = d % (M * 2 - 2);
+                        nd %= (M * 2 - 2);
                         // 이동 거리가 벽에 닿이지 않는 경우
-                        if(nd <= nx - 1){
-                            nx = nx + nd * dx[j];
-                        }else if(nd <= nx + M - 2){ // 한번 방향 튼 경우
-                            nx = 1 + (nd - (nx - 1)) * dx[j+1];
-                        }else{ // 두번 방향 튼 경우
-                            nx = M + (nd -(nx + M - 2)) * dx[j];
+                        if(nd >= nx - 1) {
+                            nd -= nx - 1;
+                            nx = 1;
+                        }else{
+                            nx += nd * dx[j];
+                            nd = 0;
                         }
+                        if(nd >= M - nx){ // 한번 방향 튼 경우
+                            nd -= M - nx;
+                            nx = M;
+                        }else{
+                            nx += nd * dx[j+1];
+                            nd = 0;
+                        }
+                        nx += nd * dx[j];
                         break;
                     case 3:
-                        nd = d % (M * 2 - 2);
+                        nd %= (M * 2 - 2);
                         // 이동 거리가 벽에 닿이지 않는 경우
-                        if(nd <= M - nx){
-                            nx = nx + nd * dx[j];
-                        }else if(nd <= M * 2 - nx - 1){ // 한번 방향 튼 경우
-                            nx = M + (nd - (M - nx)) * dx[j-1];
-                        }else{ // 두번 방향 튼 경우
-                            nx = 1 + (nd -(M * 2 - nx - 1)) * dx[j];
+                        if(nd >= M - nx){
+                            nd -= M - nx;
+                            nx = M;
+                        }else{
+                            nx += nd * dx[j];
+                            nd = 0;
                         }
+                        if(nd >= nx - 1){ // 한번 방향 튼 경우
+                            nd -= nx - 1;
+                            nx = 1;
+                        }else{ // 두번 방향 튼 경우
+                            nx += nd * dx[j-1];
+                            nd = 0;
+                        }
+                        nx += nd * dx[j];
                         break;
                 }
-                nodePq.add(new Node(ny, nx));
+                Node newNode = new Node(ny, nx);
+                 nodePq.add(newNode);
+//                if(cmp(node, newNode)) node = newNode;
             }
             // 우선순위가 높은 칸 꺼내기
-            Node node = nodePq.poll();
+             Node node = nodePq.poll();
             // 업데이트
             rabbit.y = node.y;
             rabbit.x = node.x;
 
-            int rabbitIdx = rabbit.p;
-            for(int j=1;j<=P;j++){
-                if(rabbits[j].p == rabbitIdx) continue;
-                rabbits[j].score += node.x + node.y;
-                if(rabbits[j].score > maxScore){
-                    maxScore = rabbits[j].score;
-                }
-            }
             rabbit.cnt++;
             rabbitPq.add(rabbit);
 
-            System.out.println("p: " + rabbit.p + " cnt : "  + rabbit.cnt + " score : " + rabbit.score);
-            System.out.println("y : "  + node.y + " x : " + node.x);
+//            rabbit.score -= node.x + node.y;
+//            totalSum += node.x + node.y;
+//
+//            rabbit.cnt++;
+//            rabbit.isMove = true;
+//            rabbitPq.add(rabbit);
 
+            // 실제 값 갱신
+            int nexIdx = pToIdx.get(rabbit.p);
+            point[nexIdx] = new Node(rabbit.y, rabbit.x);
+            jump[nexIdx]++;
+
+            isMove[nexIdx] = true;
+
+            result[nexIdx] -= node.x + node.y;
+            totalSum += node.x + node.y;
         }
         // 보너스 점수 받을 토끼 선정
-        Rabbit bonus = bonusRabbit.poll();
-        Rabbit bonus2 = bonusRabbit.poll();
-        bonus.score += s;
-        bonusRabbit.add(bonus);
-        bonusRabbit.add(bonus2);
-        System.out.println("p : " + bonus.p + " score : " + bonus.score);
-        System.out.println("y : "  + bonus.y + " x : " + bonus.x);
-        System.out.println("p : " + bonus2.p + " score : " + bonus2.score);
-        System.out.println("y : "  + bonus2.y + " x : " + bonus2.x);
-        if(bonus.score > maxScore){
-            maxScore = bonus.score;
+//        Rabbit bonus = new Rabbit(0, 0, 0, 0, 0, 0);
+//
+//        while(!rabbitPq.isEmpty()){
+//            Rabbit rabbit = rabbitPq.poll();
+//
+//            if(!rabbit.isMove) continue;
+//
+//            if(cmp(bonus, rabbit)) bonus = rabbit;
+//
+//        }
+//
+//        bonus.score += s;
+        Rabbit bonus = new Rabbit(0, 0, 0, 0);
+
+        while(!rabbitPq.isEmpty()){
+            Rabbit rabbit = rabbitPq.poll();
+
+            if(!isMove[pToIdx.get(rabbit.p)]) continue;
+
+            if(cmp(bonus, rabbit)) bonus = rabbit;
+
         }
+
+        result[pToIdx.get(bonus.p)] += s;
+    }
+
+    public static boolean cmp(Rabbit a, Rabbit b) {
+        if(a.x + a.y != b.x + b.y) return a.x + a.y < b.x + b.y;
+        if(a.y != b.y) return a.y < b.y;
+        if(a.x != b.x) return a.x < b.x;
+        return a.p < b.p;
+    }
+
+    public static boolean cmp(Node a, Node b) {
+        if(a.x + a.y != b.x + b.y) return a.x + a.y < b.x + b.y;
+        if(a.y != b.y) return a.y < b.y;
+        return a.x < b.x;
     }
 
     static void updateLength(int p, int L){
-        for(int j=1;j<=P;j++){
-            if(rabbits[j].p == p) {
-                rabbits[j].d *= L;
-                return;
-            }
-        }
+//        for(int i=1;i<=P;i++){
+//            if(rabbits[i].p == p) rabbits[i].d *= L;
+//            return;
+//        }
+        int idx = pToIdx.get(p);
+        d[idx] *= L;
     }
 
     static void finish(){
-        System.out.println(maxScore);
+        long ans = 0;
+        for(int i=1; i<=P; i++){
+//            ans = Math.max(ans, rabbits[i].score + totalSum);
+            ans = Math.max(ans, result[i] + totalSum);
+        }
+        System.out.println(ans);
     }
 
-    static class Rabbit{
+//    static class Rabbit{
+//        // 고유번호
+//        int p,d;
+//        // 이동횟수
+//        int cnt;
+//        long score;
+//        int y,x;
+//
+//        boolean isMove;
+//
+//        public Rabbit(int p, int d, int cnt, long score, int y, int x){
+//            this.p = p;
+//            this.d = d;
+//            this.cnt = cnt;
+//            this.score = score;
+//            this.y = y;
+//            this.x = x;
+//            this.isMove = false;
+//        }
+//    }
+
+    static class Rabbit implements Comparable<Rabbit>{
         // 고유번호
         int p;
-        // 이동거리
-        int d;
-        // 점프 횟수
+        // 이동횟수
         int cnt;
-        int score;
         int y,x;
 
-        public Rabbit(int p, int d, int cnt, int score, int y, int x){
+
+        public Rabbit(int p, int cnt, int y, int x){
             this.p = p;
-            this.d = d;
             this.cnt = cnt;
-            this.score = score;
             this.y = y;
             this.x = x;
+
+        }
+
+        @Override
+        public int compareTo(Rabbit o) {
+            if(this.cnt != o.cnt){
+                return this.cnt - o.cnt;
+            }
+            if(this.y + this.x != o.y + o.x){
+                return (this.y + this.x) - (o.y + o.x);
+            }
+            if(this.y != o.y){
+                return this.y - o.y;
+            }
+            if(this.x != o.x){
+                return this.x - o.x;
+            }
+            return this.p - o.p;
         }
     }
 
