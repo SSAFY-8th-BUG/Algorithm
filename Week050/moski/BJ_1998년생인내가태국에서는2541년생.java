acf12c4d447ccd820eb3aaf66acde43bed4272f5
9gi_coach21 <9gi_coach21@ssafy.com>
2024-01-05 11:07:31 +0900

    정경훈 : 50주차

--- Week050/moski/BJ_1998년생인내가태국에서는2541년생.java
+++ Week050/moski/BJ_1998년생인내가태국에서는2541년생.java
@@ -3,7 +3,7 @@ public class BJ_1998년생인내가태국에서는2541년생 throws Exception {
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
+        // 이거 테스트
         System.out.println(n - 543);
     }
 }
