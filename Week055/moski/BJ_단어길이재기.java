96e586da81edc736277b6cf17aa47b9c0121253c
9gi_coach21 <9gi_coach21@ssafy.com>
2024-02-07 17:41:59 +0900

    정경훈 : 55주차

---
+++ Week055/moski/BJ_단어길이재기.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ_단어길이재기 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print(br.readLine().length());
+    }
+}
